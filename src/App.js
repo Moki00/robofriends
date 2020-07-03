92850d8bba326acd12b7427256cbae0eab411462
MoKi <61506669+moki929@users.noreply.github.com>
2020-07-03 00:05:03 -0400

    daily edit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import {robots} from './robots';
+
+const state = {
+    robots: robots,
+    searchfield: ''
+}
+
+class App extends Component{
+    return (
+        <div className='tc'>
+            <h1>RoboFriends</h1>
+            <SearchBox />
+            <CardList robots={robots}/>
+        </div>
+    );
+}
+
+export default App;
