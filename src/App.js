ec3c69a6641fb230712bd6f2ee4af6ab70feb9f0
MoKi <61506669+moki929@users.noreply.github.com>
2020-07-06 09:09:01 -0400

    broken

--- src/App.js
+++ src/App.js
@@ -15,17 +15,17 @@ class App extends Component{
     }
 
     onSearchChange=(event) => {
+        this.setState({searchfield: event.target.value})
+    }
+    render() {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.searchfield.toLowerCase());
         })
-        console.log(filteredRobots);
-    }
-    render() {
         return (
             <div className='tc'>
                 <h1>RoboFriends</h1>
                 <SearchBox seachChange={this.onSearchChange}/>
-                <CardList robots={this.state.robots}/>
+                <CardList robots={filteredRobots}/>
             </div>
         );
     }
