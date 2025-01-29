9decea5ae83482c216bd8b53af410b780d3621c7
MoKi <Mokiwins@gmail.com>
2025-01-28 22:51:17 -0500

    updated gh-pages

--- src/robots.js
+++ src/robots.js
@@ -1,62 +1,62 @@
 export const robots = [
   {
     id: 1,
-    name: 'Mr Bubble Head',
-    username: 'Bret',
-    email: 'Sincere@april.biz'
+    name: "Mr Bubble Head",
+    username: "Bret",
+    email: "Sincere@april.biz",
   },
   {
     id: 2,
-    name: 'Ervin Howell',
-    username: 'Antonette',
-    email: 'Shanna@melissa.tv'
+    name: "Ervin Howell",
+    username: "Antoinette",
+    email: "Shanna@melissa.tv",
   },
   {
     id: 3,
-    name: 'Leo Queen',
-    username: 'Leozenna',
-    email: 'Leozenna@gmail.com'
+    name: "Leo Queen",
+    username: "Leozenna",
+    email: "Leozenna@gmail.com",
   },
   {
     id: 4,
-    name: 'Patricia Lebsack',
-    username: 'Karianne',
-    email: 'Julianne.OConner@kory.org'
+    name: "Patricia Lebsack",
+    username: "Karianne",
+    email: "Julianne.OConner@kory.org",
   },
   {
     id: 5,
-    name: 'Maxamillion MacKnight',
-    username: 'MaxM',
-    email: 'Maxamillion@wonder.io'
+    name: "Maxamillion MacKnight",
+    username: "MaxM",
+    email: "Maxamillion@wonder.io",
   },
   {
     id: 6,
-    name: 'Kotochan Nakanaka',
-    username: 'Kotochan',
-    email: 'Kotochan@banana.io'
+    name: "Kotochan Nakanaka",
+    username: "Kotochan",
+    email: "Kotochan@banana.io",
   },
   {
     id: 7,
-    name: 'Kurtis Weissnat',
-    username: 'Elwyn.Skiles',
-    email: 'Telly.Hoeger@billy.biz'
+    name: "Kurtis Weissnat",
+    username: "Elwyn.Skiles",
+    email: "Telly.Hoeger@billy.biz",
   },
   {
     id: 8,
-    name: 'Nicholas Runolfsdottir V',
-    username: 'Maxime_Nienow',
-    email: 'Sherwood@rosamond.me'
+    name: "Nicholas Runolfsdottir V",
+    username: "Maxime_Nienow",
+    email: "Sherwood@rosamond.me",
   },
   {
     id: 9,
-    name: 'Glenna Reichert',
-    username: 'Delphine',
-    email: 'Chaim_McDermott@dana.io'
+    name: "Glenna Reichert",
+    username: "Delphine",
+    email: "Chaim_McDermott@dana.io",
   },
   {
     id: 10,
-    name: 'Clementina DuBuque',
-    username: 'Moriah.Stanton',
-    email: 'Rey.Padberg@karina.biz'
-  }
+    name: "Clementina DuBuque",
+    username: "Moriah.Stanton",
+    email: "Rey.Padberg@karina.biz",
+  },
 ];
